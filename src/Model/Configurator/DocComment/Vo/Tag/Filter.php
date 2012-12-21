f60ce753a6ba77f99ed8ad16c7c53929a9898524
Trey Shugart <treshugart@gmail.com>
2012-12-21 16:06:23 +1100

    Update VO filter configurator code.

--- src/Model/Configurator/DocComment/Vo/Tag/Filter.php
+++ src/Model/Configurator/DocComment/Vo/Tag/Filter.php
@@ -21,23 +21,19 @@ class Filter
 
     private function parseFilterInformation($tag)
     {
-        $parts = explode(' ', $tag);
-        $parts = array_filter($parts);
+        preg_match('/(from|to) ([a-z0-9.]+) using ([a-z0-9\\\\_]+)\.?/i', $tag, $parts);
+        array_shift($parts);
 
-        if (count($parts) !== 4) {
-            throw new InvalidArgumentException('The tag "' . $tag . '" must be in the format of "@filter [to / from] [dot-notated name] using [class]".');
+        if (!$parts) {
+            throw new InvalidArgumentException('The @filter tag "' . $tag . '" must be in the format of "[to / from] [dot-notated name] using [class].".');
         }
 
         $parts[0] = strtolower($parts[0]);
 
-        if (!in_array($parts[0], ['to', 'from'])) {
-            throw new InvalidArgumentException('The direction "' . $parts[0] . '" specified for tag "' . $tag . '" is not valid.');
-        }
-
         return [
             'direction' => $parts[0],
             'name'      => $parts[1],
-            'class'     => $parts[3]
+            'class'     => $parts[2]
         ];
     }
 }
