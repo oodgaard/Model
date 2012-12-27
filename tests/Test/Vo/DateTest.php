72e251cf6ba3aea9024663f21a2e3c883de10965
Trey Shugart <treshugart@gmail.com>
2012-12-28 09:25:14 +1100

    Restructure tests to be in own VO namespace. Use UTC as the default timezone otherwise tests will fail for people in different timezones. Reuse code.

---
+++ tests/Test/Vo/DateTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Test;
+use Model\Vo\Date;
+use Testes\Test\UnitAbstract;
+
+class DateTest extends UnitAbstract
+{
+    private $time = 1262268000;
+
+    private $format = 'Y-m-d\TH:i:s\Z';
+
+    private $timezone = 'UTC';
+
+    public function setDateByInteger()
+    {
+        $date = $this->generateDate();
+        $date->set($this->time);
+        $this->assert($date->get() == '2010-01-01T00:00:00Z', 'The date was not set from an integer');
+    }
+
+    public function setDateByString()
+    {
+        $date = $this->generateDate();
+        $date->set(date('Y-m-d H:i:s', $this->time));
+        $this->assert($date->get() == '2010-01-01T00:00:00Z', 'The date was not set from a string');
+    }
+
+    public function formatDate()
+    {
+        $date = $this->generateDate('Y-m-d\TH:i:s');
+        $date->set($this->time);
+        $this->assert($date->get() == '2010-01-01T00:00:00', 'The date format was not used');
+    }
+
+    private function generateDate($format = null, $timezone = null)
+    {
+        return new Date([
+            'format'   => $format ?: $this->format,
+            'timezone' => $timezone ?: $this->timezone
+        ]);
+    }
+}
