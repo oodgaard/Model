489203fab26e216bf808408092d9f2e2b5cb4c7d
Aaron Chilcott <achilcott@ultraserve.com.au>
2013-08-21 16:50:56 +1000

    LH-480 Adds allowNull option to date VO

--- tests/Test/Vo/DateTest.php
+++ tests/Test/Vo/DateTest.php
@@ -41,16 +41,30 @@ class DateTest extends UnitAbstract
         $this->assert($date === '2009-12-31T14:00:00', 'The date format was not used');
     }
 
+    public function nullDate()
+    {
+	$expectedDate = '2009-12-31T14:00:00';
+
+        $date = $this->generateDate('Y-m-d\TH:i:s');
+        $date = $date->translate(null);
+        $this->assert($date === $expectedDate, sprintf('Unexpected return value, expected "%s" got null', $expectedDate));
+
+        $date = $this->generateDate('Y-m-d\TH:i:s', null, true);
+        $date = $date->translate(null);
+        $this->assert($date === null, sprintf('Unexpected return value, expected null got %s', get_type($date));
+    }
+
     public function tearDown()
     {
         date_default_timezone_set($this->currentTimezone);
     }
 
-    private function generateDate($format = null, $timezone = null)
+    private function generateDate($format = null, $timezone = null, $allowNull = false)
     {
         return new Date([
             'format'   => $format ?: $this->format,
-            'timezone' => $timezone ?: $this->timezone
+            'timezone' => $timezone ?: $this->timezone,
+	    'allowNull' => $allowNull,
         ]);
     }
 }
